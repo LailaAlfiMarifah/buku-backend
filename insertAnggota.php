47cbdbb2e381dc21d5be5abdf90835129826902c
Laila <12150120132@students.uin-suska.ac.id>
2023-07-04 11:58:23 +0700

    new commit

--- insertAnggota.php
+++ insertAnggota.php
@@ -1,9 +1,7 @@
 <?php
 // Include file koneksi.php untuk mendapatkan koneksi ke database
 include 'connection.php';
-header('Access-Control-Allow-Origin: http://localhost:5173');
-header('Access-Control-Allow-Methods: GET, POST');
-header('Access-Control-Allow-Headers: Content-Type');
+
 $conn = getConnection();
 
 // Mendapatkan data yang dikirim melalui metode POST
--- selectBuku.php
+++ selectBuku.php
@@ -1,8 +1,6 @@
 <?php
 include 'connection.php';
-header('Access-Control-Allow-Origin: http://localhost:5173');
-header('Access-Control-Allow-Methods: GET, POST');
-header('Access-Control-Allow-Headers: Content-Type');
+
 $connection = getConnection();
 
     if ($connection) {
